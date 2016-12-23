c9e3d0207a8809cc5f12d4d842c19d22c0d0d7b6
catriel <catriel@autocosmos.com>
2016-12-23 11:02:55 -0300

    se agrega la edad

--- Hello Wolrd.ts
+++ Hello Wolrd.ts
@@ -1,9 +1,9 @@
 function greeter(person) {
     return "Hello, " + person.name;
 }
-function impersonate(person){
-    return person.name;
+function age(person){
+    return ". You are so old, " + person.age;
 }
-var user =  {name : "World"};
+var user =  {name : "World", age:50000000};
 
 document.body.innerHTML = greeter(user); 
