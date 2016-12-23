5fd0adb6bfbc912644d189f5c28df3c3983a037f
catriel <catriel@autocosmos.com>
2016-12-23 12:16:19 -0300

    sosdios

--- helloworld.ts
+++ helloworld.ts
@@ -4,5 +4,5 @@ function greeter(person) {
 function age(person){
     return ". You are so old, " + person.age;
 }
-var user =  {name : "World", age:50000000, esjebus: true, apocaliptico: false};
+var user =  {name : "World", age:50000000, esjebus: true, apocaliptico: false, sosdios: false};
 document.body.innerHTML = greeter(user); 
